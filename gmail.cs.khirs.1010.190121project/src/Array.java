e0f4407dbb830d02db73b828e160470207deb780
302-02 <302-02@302-02-PC>
2019-01-25 15:50:50 +0900

    Array

--- gmail.cs.khirs.1010.190121project/src/Array.java
+++ gmail.cs.khirs.1010.190121project/src/Array.java
@@ -7,6 +7,8 @@ public class Array {
 		String [][] korea = 
 			{{"윤봉길", "김좌진", "김구"}, {"남자현", "유관순", "논개"}};
 		
+		
+		
 		//2개의 데이터를 이용해서 데이터 순회
 		/*
 		int rowSize = korea.length;
@@ -19,13 +21,17 @@ public class Array {
 		}
 		*/
 		
-		//int [i/3] [j%3];
+		
+		
+		//'for' '2'개를 '1'개로 줄여서 사용하는 방법
 		/*
 		int size = korea.length * korea[0].length;
 		for(int i=0; i<size; i=i+1){
 			System.out.println(korea[i/korea[0].length][i%korea[0].length]);
 		}
 		*/
+		
+		
 		
 		//빠른 열거를 이용한 배열
 		for(String [] temp : korea) {
